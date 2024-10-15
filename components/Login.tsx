bf98e28d07088c62cc35ea0b9a32144654e3c0eb
Marcos Costa <marquinho2057@gmail.com>
2024-10-15 19:22:45 -0300

    up

--- components/Login.tsx
+++ components/Login.tsx
@@ -3,46 +3,40 @@ import { useState } from "react";
 import { StyleSheet } from "react-native";
 
 export default function Login() {
-    const [password, onChangeText] = useState('');
-    const [username, onChangeUsername] = useState('');
+  const [password, onChangeText] = useState("");
+  const [username, onChangeUsername] = useState("");
 
-    return(
-        <SafeAreaView>
-            
-            <TextInput 
-            style={styles.input} 
-            onChangeText={onChangeUsername} 
-            value={username}
-            placeholder="username"
-            />
-            <TextInput 
-            style={styles.input}
-            onChangeText={onChangeText} 
-            value={password}
-            placeholder="password"
-            secureTextEntry={true}
-            />
-            <Button 
-                title='Login'
-                onPress={() => {
-                    console.log(username)
-                    console.log(password)
-                }}
-                />
-            
-           
-         </SafeAreaView>
-    
-    );
-    
-    
+  const login = () => {
+    console.log(username, password);
+  };
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeUsername}
+        value={username}
+        placeholder="username"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={password}
+        placeholder="password"
+        secureTextEntry={true}
+      />
+      <Button title="Login" onPress={login} />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-        input: {
-          height: 40,
-          margin: 12,
-          borderWidth: 1,
-          padding: 10,
-        },
-      });
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: "white",
+    borderRadius: 6,
+  },
+});
