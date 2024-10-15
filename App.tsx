bad095b6f0c7804b26e486061cc347eb0c994219
Marcos Costa <marquinho2057@gmail.com>
2024-10-15 19:35:35 -0300

    up1

--- App.tsx
+++ App.tsx
@@ -1,24 +1,19 @@
-
-import { StyleSheet, Text, View } from 'react-native';
-import Login from './components/Login';
+import { StyleSheet, Text, View } from "react-native";
+import Login from "./components/Login";
 
 export default function App() {
-  
-  
   return (
     <View style={styles.container}>
       <Login></Login>
-
     </View>
-  
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
